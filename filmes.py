9e06c1eaa54cab0fde2c33d2c4a4ccdd598f7155
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-04-08 23:57:29 -0300

    Adicionando mais funcionalidades ao projeto

---
+++ filmes.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+class Filme:
+    def __init__(self, nome, genero, plataforma):
+        self.nome = nome
+        self.genero = genero
+        self.plataforma = plataforma
+
+#Lista de Filmes
+filme1 = Filme('Titanic', 'Romance', 'Disney+')
+filme2 = Filme('Interestelar', 'Ficção científica', 'Max')
+filme3 = Filme('Ilha do medo', 'Suspense', 'Prime Video')
+lista = [filme1, filme2, filme3]
+
+app = Flask(__name__)
+app.secret_key = 'cassics'
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Filmes', filmes=lista )
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Filme')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    genero = request.form['genero']
+    plataforma = request.form['plataforma']
+    filme = Filme(nome, genero, plataforma)
+    lista.append(filme)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado!')
+        return redirect('/login')
+
+    
+app.run(debug=True) 
